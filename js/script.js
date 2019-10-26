28ac626199a57f26ddaf9802664e146eae61a93b
PatrykPorabik <patryk@porabik.pl>
2019-10-26 18:02:40 +0200

    fix js - validate email format and add success msg

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 let formEmail = document.getElementById('newsletter-form'),
-    msg = document.querySelector('.success'),
-    errormsg = document.querySelector('.none');
+    errormsg = document.querySelector('.none'),
+    successmsg = document.getElementById('successmsg');
 
 function validateEmail(field) {
     const regex = /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
@@ -16,7 +16,7 @@ function validateEmail(field) {
 function isValidEmail(field) {
     if (field === "") return false;
 
-    if (validateEmailField(field.value)) {
+    if (validateEmail(field.value)) {
         field.classList.add('newsletter__input--valid');
         field.classList.remove('newsletter__input--invalid');
         errormsg.classList.add('none');
@@ -32,10 +32,14 @@ function isValidEmail(field) {
 
 formEmail.addEventListener("submit", function (e) {
     e.preventDefault();
-    let email = document.getElementById('button');
+    const email = document.getElementById('email');
     if (isValidEmail(email)) {
-       //todo
+        formEmail.classList.add('none');
+        successmsg.classList.remove('none');
+        successmsg.classList.add('visible');
     } else {
          //todo
     }
 });
+
+//Todo Backend - Collect emails
